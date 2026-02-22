46410bbe105c91f69cc8451e7d49fe8c8e543d40
Muhammad-Anas16 <anas.hanf38@gmail.com>
2026-02-22 19:35:05 +0500

    start working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router';
 import './App.css'
 import HomePage from './component/page/HomePage';
 
--- src/component/page/HomePage.jsx
+++ src/component/page/HomePage.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { MdOutlineInventory2 } from "react-icons/md";
 
 const HomePage = () => {
   return (
     <div>
-      
+      <div>
+        <MdOutlineInventory2 />
+      </div>
+      <h1>InventoryPro</h1>
+      <p>Your comprehensive offline inventory solution.</p>
+      <div>
+        <button> Create Local Workspace</button>
+        <button>Import Existing Data</button>
+      </div>
+      <div>Your data is stored locally on this device.</div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
